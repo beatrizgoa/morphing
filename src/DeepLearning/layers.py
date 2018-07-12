c8e7099c25ccd80622d85f53ad38f4dd85c1a79f
gomezabeatriz <beaa2509@gmail.com>
2018-07-12 15:30:13 +0200

    Some errors solved

--- src/DeepLearning/layers.py
+++ src/DeepLearning/layers.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 
-def convLayer(nameLayer,n_kernels, size_kernels, x, strides = (1,1,1,1)):
-	# COnvolutional layer is defined	
+
+def convLayer(nameLayer,x, n_channels, n_kernels, size_kernels, strides = (1,1,1,1)):
+	# COnvolutional layer is defined
 	with tf.name_scope(nameLayer):
-		w = tf.variable(tf.truncated_normal(shape=size_kernels, n_kernels, mean = 0, stddev = 0.1, name = nameLayer)
-		b = tf.Variable(tf.zeros(n_kernels)
+		w = tf.Variable(tf.truncated_normal(shape = [size_kernels, size_kernels, n_channels, n_kernels], mean = 0, stddev = 0.1, name = nameLayer))
+		b = tf.Variable(tf.zeros(n_kernels))
 		conv_op = tf.nn.conv2d(x, w, strides = strides, padding = 'VALID')
-		activation = tf.nn.relu(conv) + b
-		
+		activation = tf.nn.relu(conv_op) + b
+
 		# define summaries
 		tf.summary.histogram("weights", w)
 		tf.summary.histogram("bias", b)
@@ -15,23 +16,24 @@ def convLayer(nameLayer,n_kernels, size_kernels, x, strides = (1,1,1,1)):
 
 		return conv_op, activation
 
+
 def poolLayer(nameLayer, x, ksize, strides= (1,1,1,1)):
 	# max pool layer is defined
-	with tf.name_scoope(nameLayer):
-		return tf.nn.maxpool(x, ksize = ksize, strides = strides, padding="SAME") 
+	with tf.name_scope(nameLayer):
+		return tf.nn.max_pool(x, ksize = ksize, strides = strides, padding="SAME")
 		
 
-def hiddenLayer(nameLayer, x, n_neurons):
+
+def hiddenLayer(nameLayer, x, in_neurons, out_neurons):
 	# Hidden Layer is define
 	with tf.name_scope(nameLayer):
-		w = tf.Variable(tf.truncated_normal([input_neurons, output_neurons], stddev = 0.1, name = "w")
-		b = tf.Variable(tf.truncated_normal(tf.zeros(output_neurons))
-		activation = tf.matmul(x,w) + b
+		w = tf.Variable(tf.truncated_normal([in_neurons, out_neurons], stddev = 0.1, name = "w"))
+		b = tf.Variable(tf.truncated_normal(tf.zeros(out_neurons)))
+		activation = tf.matmul(x ,w) + b
 
 		tf.summary.histogram("weights", w)
 		tf.summary.histogram("bias", b)
 		tf.summary.histogram("activattions", activation)
-
 
 	return activation
 
